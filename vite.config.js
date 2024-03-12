7dc55f9351e2a9abba4451f2ab88f6413de19519
SvenK <git+sveto@svenk.org>
2024-03-12 14:49:42 +0100

    Further cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 anabrid GmbH
+// Contact: https://www.anabrid.com/licensing/
+// SPDX-License-Identifier: MIT OR GPL-2.0-or-later
+
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
